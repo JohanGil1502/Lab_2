161b07c5b2cee157055b2dca33e3f8c01d8c9876
SebasGrosso <sebastian.gil01@uptc.edu.co>
2024-10-14 23:36:00 -0500

    Se agrega la interfaz del monitor

--- Middleware/monitor/public/monitor_front.js
+++ Middleware/monitor/public/monitor_front.js
@@ -1,13 +1,29 @@
-const { createApp, ref } = Vue;
-var socket = io.connect('http://localhost:7000', { 'forceNew': true });
-
-createApp({
+const app = new Vue({
+    el: '#app',
     data() {
         return {
-            letter: ''  // letter ahora es reactiva
-        }
+            overallStatusColor: 'green',
+            servers: [
+                {
+                    name: 'Server 1',
+                    status: ['green', 'green', 'yellow', 'red', 'green', 'green'],
+                    uptime: 99.96
+                },
+                {
+                    name: 'Server 2',
+                    status: ['green', 'green', 'green', 'yellow', 'green'],
+                    uptime: 99.99
+                }
+            ],
+            socket: null
+        };
     },
     methods: {
+        stateColor(state) {
+            if (state === 'green') return 'green';
+            if (state === 'red') return 'red';
+            if (state === 'yellow') return 'yellow';
+        },
         async deployServer() {
             const response = await fetch(`http://localhost:7000/deploy`, {
                 method: 'GET',
@@ -24,10 +40,19 @@ createApp({
         }
     },
     mounted() {
-        var self = this;  // Guardar referencia al contexto de Vue
-        socket.on('messages', function (data) {
-            self.letter = self.letter+data;  // Actualizar 'letter' de forma reactiva
+        this.socket = io.connect('http://localhost:7000', { 'forceNew': true });
+        
+        this.socket.on('connect', () => {
+            console.log('Conectado al servidor de WebSocket');
+        });
+
+        this.socket.on('connect_error', (err) => {
+            console.error('Error de conexión:', err);
+        });
+
+        this.socket.on('messages', (data) => {
             console.log(data);
+            // Aquí puedes actualizar el estado de tus servidores según el mensaje recibido
         });
     }
-}).mount("#app");
+});
